31150d8ae8d205de0028282babbcde982b7aa0e1
tanipa1 <tania35.291@gmail.com>
2023-05-02 21:41:07 +0600

    Data Found

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
+cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipe.json');
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Mangia Italiano is Running')
+})
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs)
+})
+
+app.get('/recipes', (req, res) => {
+  res.send(recipes)
 })
 
 app.listen(port, () => {
